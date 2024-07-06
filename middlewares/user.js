51c47d5b435125363b13df8cb87ba830d2f4ca60
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-06 18:24:36 +0530

    update

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,6 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export function userMiddleware(req, res, next) {
+  const { UserName, Email, Password } = req.body;
+  if (!UserName || !Email || !Password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  next();
   const token = req.headers.authorization;
 
   if (!token) {
@@ -8,22 +13,24 @@ export function userMiddleware(req, res, next) {
       message: "Error in parsing the token, Please try again.",
     });
   }
+
   const jwttoken = token.split(" ")[1];
+
   try {
     const token_verify = jwt.verify(jwttoken, process.env.JWT_SECRET_KEY);
     if (token_verify) {
-      req.header = token_verify.userid;
+      // Attach user ID to request header for further use
+      req.userId = token_verify.userid;
       next();
     } else {
-      return res.json({
+      return res.status(401).json({
         message: "You are not authorized",
       });
     }
   } catch (error) {
-    console.log(error);
-    return res.json({
-      message:
-        "Something went wrong while validating the user, Please try again.",
+    console.error("JWT Verification Error:", error);
+    return res.status(500).json({
+      message: "Error validating user token, Please try again.",
     });
   }
 }
